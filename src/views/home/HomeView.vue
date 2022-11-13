ae35709caf9077300e65e817a8168f0631f82601
huitoutunao <lh_huitoutunao@163.com>
2022-11-13 20:39:59 +0800

    feat: 新增pinia

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="page">hello vue3</div>
+  <div @click="counter.increment()">
+    {{ countValue }}
+  </div>
 </template>
 
 <script setup lang="ts">
-console.log('home')
+import { storeToRefs } from 'pinia'
+import { useCounterStore } from '@/store'
+
+const counter = useCounterStore()
+const { countValue } = storeToRefs(counter)
 </script>
 
 <style scoped>
